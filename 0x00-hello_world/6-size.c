668127aca42742900d564cd5c3adbd874e501da8
Sunday <saborishade@gmail.com>
2022-09-10 08:15:18 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	double d;
+
 	printf("size of int: %ld byte(s)\n", sizeof(i));
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of long: %ld byte(s)\n", sizeof(li));
 	printf("size of long long: %ld byte(s)\n", sizeof(lli));
 	printf("size of float: %ld byte(s)\n", sizeof(f));
-	printf("size of double: %ld byte(s)\n", sizeof(d));
 	return (0);
 }
