9ae090fe0cef7fcb4dcca773369bdd34f339bc2e
Sunday <saborishade@gmail.com>
2022-09-12 07:40:41 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	char x, y;
+
+	for (x = 'a'; y = 'A'; 'a' <= 'z'; 'A' <= 'Z'; a++; A++)
+	{
+		putchar(x);
+		putchar(y);
+	}
+	{
+		putchar('\n')
+	}
+	return (0);
+}
+
