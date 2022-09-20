4ffc026b88014482ce5b88c05d8d5499404c1918
Sunday <saborishade@gmail.com>
2022-09-19 17:04:06 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 /**
- * puts2 -  function that prints every other character of a string
+ * puts2 - function that prints every other character of a string
  * @str: string
  *
  * Return: nothing
  */
 void puts2(char *str)
 {
-	int i = 0
+	int i = 0;
 
 		while (*(str + i) != '\0')
 		{
