59bf50737639642303d6bff9223ec297f6fdb711
Sunday <saborishade@gmail.com>
2022-09-19 21:00:18 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,12 +4,13 @@
 /**
  * print_rev - prints a string, in reverse, followed by
  * a new line.
- * @str: reverse order string
- *@s: string
+ * @s: reverse order string
+ *
  * Return: return nothing
  */
 void print_rev(char *s)
-{int len = strlen(s);
+{
+	int len = strlen(s);
 
 	while (len--)
 		_putchar(*(s + len));
