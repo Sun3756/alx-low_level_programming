65d3950d0a60e72faf93c03433ffe7dff5d3a10d
Sunday <saborishade@gmail.com>
2022-09-22 16:14:03 -0700

    tast 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately
  */
- int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
 	while (s1[a] == s2[a] && s1[a] && s2[a])
-		{
-			a++;
-		}
-		return (s1[a] - s2[a]);
+	{
+		a++;
+	}
+	return (s1[a] - s2[a]);
 }
